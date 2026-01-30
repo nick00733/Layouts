b248ff78c714c74fda8bb39601a01f207a6371c0
Nick Nedjat <nick.nedjat@icloud.com>
2026-01-30 18:23:04 -0500

    FUndamentals 2

--- Layouts/ContentView.swift
+++ Layouts/ContentView.swift
@@ -3,19 +3,37 @@
 //  Layouts
 //
 //  Created by Nick Nedjat on 30.01.26.
-//
 
 import SwiftUI
 
 struct ContentView: View {
+    @State private var messageString =
+        "When the Genius Bar Needs Help, They Call You!"
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text(messageString)
+                .font(.largeTitle)
+                .fontWeight(.heavy)
+                .minimumScaleFactor(0.5)
+                .multilineTextAlignment(.center)
+                .foregroundStyle(.red)
+                .frame(height: 150)
+                .frame(maxWidth: .infinity)
+                .border(.orange, width: 2)
+                .padding()
+
+            HStack {
+                Button("Awesome") {
+                    messageString = "You Are Awesome!"
+                }
+
+                Button("Great") {
+                    messageString = "You Are Great!"
+                }
+            }
+            .buttonStyle(.borderedProminent)
         }
-        .padding()
     }
 }
 
